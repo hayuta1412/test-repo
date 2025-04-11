333619025e8b972e7a3387b33fb8cacdf4555129
Nguyen Phu Quan <quannp3@vmogroup.com>
2025-04-11 23:30:12 +0700

    Merge Squash 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
       <p>Test Commit 2</p>
       <p>Test Squash 1</p>
       <p>Test Squash 223232</p>
+      <p>Test Squash kljlkasd</p>
+      <p>Test Squash sdf</p>
     </div>
   );
 }

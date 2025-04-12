2f776918a7d20ec68ec049907ed0667668525852
Nguyen Phu Quan <quannp3@vmogroup.com>
2025-04-12 10:28:08 +0700

    commit newbranch 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
       <p>Test Commit 1</p>
       <p>Test Commit 2</p>
       <p>Test Squash 1</p>
+      <p>Test comit </p>
+      <p>Test comit dsfasdf</p>
+      <p>Test comit dsfasdf</p>
+      <p>Test comit dsfasdf</p>
     </div>
   );
 }
